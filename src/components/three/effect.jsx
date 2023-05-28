69109130d5343bd03123f7a29f75b66fa0574a0c
unknown <partharora557@gmail.com>
2023-05-29 00:37:19 +0530

    done

--- src/components/three/effect.jsx
+++ src/components/three/effect.jsx
@@ -1,8 +1,12 @@
-import { useLoader } from '@react-three/fiber'
+import { EffectComposer, SSR, Bloom, LUT } from '@react-three/postprocessing'
 
-export  default function Effects() {
-
+export default function Effects() {
   return (
- <></>
+    enabled && (
+      <EffectComposer disableNormalPass>
+        <Bloom luminanceThreshold={0.5} mipmapBlur luminanceSmoothing={0} intensity={1.5} />
+        {/* <LUT lut={texture} /> */}
+      </EffectComposer>
+    )
   )
 }
