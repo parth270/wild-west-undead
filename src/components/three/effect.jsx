6afd1e25eea3cd7503538488dffa70a07dfbd427
unknown <partharora557@gmail.com>
2023-05-29 00:43:02 +0530

    done

--- src/components/three/effect.jsx
+++ src/components/three/effect.jsx
@@ -1,12 +1,15 @@
-import { EffectComposer, SSR, Bloom, LUT } from '@react-three/postprocessing'
+import { EffectComposer, SSR, Bloom, LUT } from "@react-three/postprocessing";
 
 export default function Effects() {
   return (
-    enabled && (
-      <EffectComposer disableNormalPass>
-        <Bloom luminanceThreshold={0.5} mipmapBlur luminanceSmoothing={0} intensity={1.5} />
-        {/* <LUT lut={texture} /> */}
-      </EffectComposer>
-    )
-  )
+    <EffectComposer disableNormalPass>
+      <Bloom
+        luminanceThreshold={0.5}
+        mipmapBlur
+        luminanceSmoothing={0}
+        intensity={1.5}
+      />
+      {/* <LUT lut={texture} /> */}
+    </EffectComposer>
+  );
 }
